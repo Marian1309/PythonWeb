60d2477b1b4a114bbc31adcbcf92e013f5da3b4c
Marian <pidchashymaryan@gmail.com>
2023-10-25 15:28:55 +0300

    organised app structure? added run.py to run application

---
+++ lab3/app/views.py
@@ -0,0 +1,51 @@
+import platform
+from datetime import datetime
+
+from lab3.data import data
+from flask import request, render_template, Flask
+
+app = Flask(__name__)
+
+
+@app.context_processor
+def base():
+    now = datetime.now()
+    time = now.strftime("%d/%m/%y %H:%M:%S")
+    return dict(
+        platform=platform,
+        agent=request.user_agent,
+        time=time)
+
+
+@app.route("/")
+def index():
+    title = "Home"
+    return render_template("index.html", title=title)
+
+
+@app.route("/about")
+def about():
+    title = "About"
+    return render_template("about.html", title=title)
+
+
+@app.route("/contact")
+def contact():
+    title = "Contact"
+    return render_template("contact.html", title=title)
+
+
+@app.route("/albums")
+def albums():
+    title = "Albums"
+    return render_template("albums.html", title=title)
+
+
+@app.route("/skills/")
+@app.route("/skills/<int:s_id>/")
+def skills(s_id=None):
+    title = "My skills"
+    if s_id is not None:
+        title = title.rstrip("s")
+    my_skills = data.my_skills
+    return render_template("skills.html", title=title, my_skills=my_skills, s_id=s_id)
---
+++ lab3/data/data.py
@@ -0,0 +1,10 @@
+my_skills = {
+        1: [
+            "Programming", "Languages: Python, C++, JavaScript, PHP, Java\
+            Frameworks: Flask, Django, Spring"],
+        2: ["Running", "Skills 2"],
+        3: ["Linguistics", "Skill 3"],
+        4: ["Soft skills", "Skill 3"],
+        5: ["Fast reading and comprehension", "Skill 5"],
+        6: ["Tinkering", "Skill 6"]
+}
